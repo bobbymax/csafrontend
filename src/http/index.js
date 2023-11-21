a32ea0400d4a2fd92ea4b4e175defc26da03ea49
Bobby Ekaro <bobby.ekaro@ncdmb.gov.ng>
2023-11-21 10:31:44 +0100

    headers

--- src/http/index.js
+++ src/http/index.js
@@ -2,6 +2,10 @@ import axios from "axios"
 
 export default axios.create({
     baseURL: process.env.REACT_APP_BASE_URI,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
 })
 
 export const axiosPrivate = axios.create({
